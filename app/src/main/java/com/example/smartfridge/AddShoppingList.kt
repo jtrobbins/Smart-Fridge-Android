95bb97998f2e2a325b027968ad3af1392be3cf40
Jeffrey Robbins <jrobbins@terpmail.umd.edu>
2020-10-18 15:44:57 -0400

    add shopping list added

--- app/src/main/java/com/example/smartfridge/AddShoppingList.kt
+++ app/src/main/java/com/example/smartfridge/AddShoppingList.kt
@@ -1,37 +1,52 @@
 package com.example.smartfridge
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.DialogFragment
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class ShoppingListsActivity : AppCompatActivity() {
+class AddShoppingList : AppCompatActivity() {
 
     private lateinit var mDialog: DialogFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.shopping_lists)
+        setContentView(R.layout.add_shopping_list)
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
-        supportActionBar?.title = "Shopping Lists"
+        supportActionBar?.title = "Add Shopping List"
 
-        toolbar.setNavigationOnClickListener {
+        var mTitleText = findViewById<View>(R.id.shopping_list_title) as EditText
+        var mDescriptionText = findViewById<View>(R.id.shopping_list_description) as EditText
+
+        val cancelButton = findViewById<View>(R.id.cancelButton) as Button
+        cancelButton.setOnClickListener {
+            val data = Intent()
+            setResult(Activity.RESULT_CANCELED, data)
             finish()
         }
 
-        val fab = findViewById<FloatingActionButton>(R.id.fab)
-        fab.setOnClickListener {
-            val addShoppingListIntent = Intent(this, AddShoppingList::class.java)
-            startActivity(addShoppingListIntent)
+        val resetButton = findViewById<View>(R.id.resetButton) as Button
+        resetButton.setOnClickListener {
+            mTitleText.setText("")
+            mDescriptionText.setText("")
         }
+
+        val submitButton = findViewById<View>(R.id.submitButton) as Button
+        submitButton.setOnClickListener {
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -39,10 +54,11 @@ class ShoppingListsActivity : AppCompatActivity() {
         return true
     }
 
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.action_more_information -> {
-                mDialog = DialogFragmentShoppingListsActivity.newInstance()
+                mDialog = DialogFragmentAddShoppingList.newInstance()
                 mDialog.show(supportFragmentManager, TAG)
                 true
             }
---
+++ app/src/main/java/com/example/smartfridge/DialogFragmentAddShoppingList.kt
@@ -0,0 +1,23 @@
+package com.example.smartfridge
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+
+class DialogFragmentAddShoppingList : DialogFragment() {
+
+    companion object {
+
+        fun newInstance(): DialogFragmentAddShoppingList {
+            return DialogFragmentAddShoppingList()
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return AlertDialog.Builder(activity)
+            .setTitle("Lorem Ipsum")
+            .setMessage("Lorem Ipsum.")
+            .create()
+    }
+}
