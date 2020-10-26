03b7c6307a9b3def89390cf4700448f9746cc510
Jeffrey Robbins <jrobbins@terpmail.umd.edu>
2020-10-26 14:04:23 -0400

    Recipes Autocomplete search added

--- app/src/main/java/com/example/smartfridge/recipes/RecipesActivity.kt
+++ app/src/main/java/com/example/smartfridge/recipes/RecipesActivity.kt
@@ -3,18 +3,28 @@ package com.example.smartfridge.recipes
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.DialogFragment
 import com.example.smartfridge.R
 
+
 class RecipesActivity : AppCompatActivity() {
 
     private lateinit var mDialog: DialogFragment
+    private lateinit var searchView: AutoCompleteTextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.recipes)
+
+        searchView = findViewById(R.id.autocomplete_recipe)
+
+        val recipesList = arrayOf("Coconut Cake", "Homemade Lasagna", "Stuffed Bell Peppers", "French Toast", "Broccoli Salad")
+        val autocompleteAdapter: ArrayAdapter<String> = ArrayAdapter<String>(this, R.layout.autocomplete_item, recipesList)
+        searchView.setAdapter(autocompleteAdapter)
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
