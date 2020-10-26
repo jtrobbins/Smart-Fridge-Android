bcb119e9c9da61fbd421e7f4ebb41aab75214770
Jeffrey Robbins <jrobbins@terpmail.umd.edu>
2020-10-26 14:35:35 -0400

    Recipes Autocomplete search activity link

--- app/src/main/java/com/example/smartfridge/recipes/RecipesActivity.kt
+++ app/src/main/java/com/example/smartfridge/recipes/RecipesActivity.kt
@@ -1,14 +1,21 @@
 package com.example.smartfridge.recipes
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
 import android.view.Menu
 import android.view.MenuItem
+import android.view.inputmethod.EditorInfo
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.DialogFragment
 import com.example.smartfridge.R
+import com.example.smartfridge.main.NotImplementedActivity
+import com.example.smartfridge.shoppinglists.AddShoppingList
+import com.example.smartfridge.shoppinglists.ShoppingListsActivity
 
 
 class RecipesActivity : AppCompatActivity() {
@@ -23,8 +30,35 @@ class RecipesActivity : AppCompatActivity() {
         searchView = findViewById(R.id.autocomplete_recipe)
 
         val recipesList = arrayOf("Coconut Cake", "Homemade Lasagna", "Stuffed Bell Peppers", "French Toast", "Broccoli Salad")
-        val autocompleteAdapter: ArrayAdapter<String> = ArrayAdapter<String>(this, R.layout.autocomplete_item, recipesList)
+        val autocompleteAdapter: ArrayAdapter<String> = ArrayAdapter(this, R.layout.autocomplete_item, recipesList)
         searchView.setAdapter(autocompleteAdapter)
+
+        searchView.setOnEditorActionListener { v, actionId, event ->
+            if ((event != null && (event.keyCode == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+
+                if (searchView.text.toString() == "Coconut Cake") {
+                    val coconutCakeIntent = Intent(this, NotImplementedActivity::class.java)
+                    startActivity(coconutCakeIntent)
+
+                } else if (searchView.text.toString() == "Homemade Lasagna") {
+                    val homemadeLasagnaIntent = Intent(this, NotImplementedActivity::class.java)
+                    startActivity(homemadeLasagnaIntent)
+
+                } else if (searchView.text.toString() == "Stuffed Bell Peppers") {
+                    val stuffedBellPeppersIntent = Intent(this, NotImplementedActivity::class.java)
+                    startActivity(stuffedBellPeppersIntent)
+
+                } else if (searchView.text.toString() == "French Toast") {
+                    val frenchToastIntent = Intent(this, NotImplementedActivity::class.java)
+                    startActivity(frenchToastIntent)
+
+                } else if (searchView.text.toString() == "Broccoli Salad") {
+                    val broccoliSaladIntent = Intent(this, NotImplementedActivity::class.java)
+                    startActivity(broccoliSaladIntent)
+                }
+            }
+            false
+        }
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
