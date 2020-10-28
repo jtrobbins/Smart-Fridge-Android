5acb8b77d82774bd6ebc2c20d15456e8c3c34898
Jeffrey Robbins <jrobbins@terpmail.umd.edu>
2020-10-28 15:19:03 -0400

    not implemented activity updated

--- app/src/main/java/com/example/smartfridge/main/NotImplementedActivity.kt
+++ app/src/main/java/com/example/smartfridge/main/NotImplementedActivity.kt
@@ -2,11 +2,22 @@ package com.example.smartfridge.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.smartfridge.R
 
 class NotImplementedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.not_implemented)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.title = "Not Implemented"
+
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
     }
 }
