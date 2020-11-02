bf00926f3aa46ff79d9ce86d8c4bb41e5bd480aa
Jeffrey Robbins <jrobbins@terpmail.umd.edu>
2020-11-02 01:25:23 -0500

    Broccoli Salad Recipe Complete

--- app/src/main/java/com/example/smartfridge/recipes/BroccoliSaladActivity.kt
+++ app/src/main/java/com/example/smartfridge/recipes/BroccoliSaladActivity.kt
@@ -1,13 +1,32 @@
 package com.example.smartfridge.recipes
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.DialogFragment
 import com.example.smartfridge.R
 
 class BroccoliSaladActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.not_implemented)
+        setContentView(R.layout.recipe_broccoli_salad)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.title = "Broccoli Salad"
+
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
+
+    }
+
+    companion object {
+        private const val TAG = "SmartFridge:RecipesActivity:BroccoliSalad"
     }
 }
