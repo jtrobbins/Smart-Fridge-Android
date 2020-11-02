e1be8241ee990cace59d2c221c4b4b5356139820
Jeffrey Robbins <jrobbins@terpmail.umd.edu>
2020-11-02 02:04:38 -0500

    French Toast Recipe Complete

--- app/src/main/java/com/example/smartfridge/recipes/FrenchToastActivity.kt
+++ app/src/main/java/com/example/smartfridge/recipes/FrenchToastActivity.kt
@@ -2,12 +2,28 @@ package com.example.smartfridge.recipes
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.smartfridge.R
 
 class FrenchToastActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.not_implemented)
+        setContentView(R.layout.recipe_french_toast)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.title = "French Toast"
+
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
+
+    }
+
+    companion object {
+        private const val TAG = "SmartFridge:RecipesActivity:FrenchToast"
     }
 }
