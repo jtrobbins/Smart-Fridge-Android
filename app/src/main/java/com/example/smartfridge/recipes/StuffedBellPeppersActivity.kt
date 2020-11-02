227824592d2c3406fdaea10a436f8bcde5e3d599
Jeffrey Robbins <jrobbins@terpmail.umd.edu>
2020-11-02 01:58:03 -0500

    Stuffed Bell Peppers Recipe Complete

--- app/src/main/java/com/example/smartfridge/recipes/StuffedBellPeppersActivity.kt
+++ app/src/main/java/com/example/smartfridge/recipes/StuffedBellPeppersActivity.kt
@@ -2,12 +2,28 @@ package com.example.smartfridge.recipes
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.smartfridge.R
 
 class StuffedBellPeppersActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.not_implemented)
+        setContentView(R.layout.recipe_stuffed_bell_peppers)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.title = "Stuffed Bell Peppers"
+
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
+
+    }
+
+    companion object {
+        private const val TAG = "SmartFridge:RecipesActivity:StuffedBellPeppers"
     }
 }
