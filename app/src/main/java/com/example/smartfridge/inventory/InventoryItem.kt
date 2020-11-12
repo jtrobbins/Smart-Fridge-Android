45339bc1c6c9cb331b182f4612b2ae728d4dea40
Jeffrey Robbins <jrobbins@terpmail.umd.edu>
2020-11-11 23:15:39 -0500

    inventory fixes

--- app/src/main/java/com/example/smartfridge/inventory/InventoryItem.kt
+++
@@ -1,6 +0,0 @@
-package com.example.smartfridge.inventory
-
-import java.util.ArrayList
-
-data class InventoryItem (var itemName: String = "", var itemQuantity : String = "",
-                            var itemExpDate : String = "")
